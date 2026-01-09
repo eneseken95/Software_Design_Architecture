fe30b1ecdf9038bfceb40517badafab3c398778a
Enes Eken <144843964+eneseken95@users.noreply.github.com>
2026-01-09 22:02:09 +0300

    Add Stock Tracking Observer Example

--- Topics/Software_Design_Architecture.playground/Pages/Observer_Pattern.xcplaygroundpage/Contents.swift
+++ Topics/Software_Design_Architecture.playground/Pages/Observer_Pattern.xcplaygroundpage/Contents.swift
@@ -2,6 +2,8 @@ import Foundation
 
 // MARK: - Observer Pattern
 
+// MARK: Agency Notification Observer
+
 protocol Observer: AnyObject {
     func update(_ message: String)
 }
@@ -42,8 +44,6 @@ final class Subscriber: Observer {
     }
 }
 
-// MARK: - Demo
-
 let agency = NewsAgency()
 let enes = Subscriber(name: "enes")
 let efe = Subscriber(name: "efe")
@@ -53,3 +53,83 @@ agency.attach(efe)
 agency.notify("New iOS 26 Released!")
 
 print(agency.observers)
+
+// MARK: Stock Tracking Observer
+
+protocol StockObserver: AnyObject {
+    func update(_ newPrice: Double)
+}
+
+protocol StockSubject {
+    func registerObserver(_ o: StockObserver)
+    func removeObserver(_ o: StockObserver)
+    func notifyObservers()
+}
+
+final class Stock: StockSubject {
+    private var observers: [StockObserver] = []
+    private let name: String
+    private var price: Double
+
+    init(_ name: String, _ price: Double) {
+        self.name = name
+        self.price = price
+    }
+
+    func registerObserver(_ o: StockObserver) {
+        observers.append(o)
+    }
+
+    func removeObserver(_ o: StockObserver) {
+        observers.removeAll { $0 === o }
+    }
+
+    func setPrice(_ price: Double) {
+        self.price = price
+        notifyObservers()
+    }
+
+    func notifyObservers() {
+        for o in observers {
+            o.update(price)
+        }
+    }
+}
+
+final class MobileClient: StockObserver {
+    private let username: String
+
+    init(username: String) {
+        self.username = username
+    }
+
+    func update(_ newPrice: Double) {
+        print("\(username) updated stock price to \(newPrice)")
+    }
+}
+
+final class WebClient: StockObserver {
+    private let dashboardName: String
+
+    init(dashboardName: String) {
+        self.dashboardName = dashboardName
+    }
+
+    func update(_ newPrice: Double) {
+        print("\(dashboardName) updated stock price to \(newPrice)")
+    }
+}
+
+let btc = Stock("BTC", 65.000)
+
+let mobile: StockObserver = MobileClient(username: "Enes")
+let web: StockObserver = WebClient(dashboardName: "Enes' Dashboard")
+
+btc.registerObserver(mobile)
+btc.registerObserver(web)
+
+btc.setPrice(100)
+btc.setPrice(90)
+
+btc.removeObserver(mobile)
+btc.removeObserver(web)
